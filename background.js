8da4a757cb78b47d41cb671f5af1ad5f5d4a5106
Josef Cech <czech.josef@gmail.com>
2016-12-18 18:51:36 +0100

    Added browser action to enable/disable feature

--- background.js
+++ background.js
@@ -1,12 +1,56 @@
 
-function cancel(requestDetails) {
+function cancelRequest(requestDetails) {
   console.log("Blocking: " + requestDetails.url);
   return { cancel: true };
 }
 
-var blockedUrls = [
-  "*://*.facebook.com/*"
-];
+function refreshBlocker(enabled, blockedUrls) {
+  // set correct browser action icon
+  browser.browserAction.setIcon({ path: enabled ? "icons/browser-action-enabled.png" : "icons/browser-action-disabled.png" });
 
+  // refresh listener
+  browser.webRequest.onBeforeRequest.removeListener(cancelRequest);
+  if (enabled && blockedUrls.length) {
+    browser.webRequest.onBeforeRequest.addListener(cancelRequest, { urls: blockedUrls }, [ "blocking" ]);
+  }
+}
 
-browser.webRequest.onBeforeRequest.addListener(cancel, { urls: blockedUrls }, [ "blocking" ]);
+function initialize(settings) {
+  function saveSettings() {
+    browser.storage.local.set({ settings : {
+      enabled: enabled,
+      blockedUrls: blockedUrls
+    }});
+  }
+
+  var enabled = settings.enabled;
+  var blockedUrls = settings.blockedUrls;
+
+  refreshBlocker(enabled, blockedUrls);
+
+  browser.browserAction.onClicked.addListener(function () {
+    enabled = ! enabled;
+    saveSettings();
+    refreshBlocker(enabled, blockedUrls);
+  });
+}
+
+var defaultSettings = {
+  enabled: true,
+  blockedUrls: [
+    "*://*.facebook.com/*"
+  ]
+}
+var settingsPromise = browser.storage.local.get("settings");
+settingsPromise.then(function (item) {
+  if (item.settings && item.settings[0]) {
+    initialize(item.settings[0]);
+  } else if (item.settings) {
+    initialize(item.settings);
+  } else {
+    initialize(defaultSettings);
+  }
+}, function (error) {
+  console.log(`Error: ${error}`);
+  initialize(defaultSettings);
+});
